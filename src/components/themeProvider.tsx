d0ce6e7c59a3471954bc0502645a233d0b02a71f
Dmitry Karpov <karpovdu.01@gmail.com>
2025-08-03 20:17:17 +0500

    fix ThemeProvider

--- src/components/themeProvider.tsx
+++ src/components/themeProvider.tsx
@@ -1,11 +1,7 @@
 import { createTheme, CssBaseline, ThemeProvider as MuiThemeProvider } from "@mui/material"
 import type { ReactNode } from "react"
 
-const theme = createTheme({
-  typography: {
-    fontFamily: "Roboto, Arial, sans-serif",
-  },
-})
+const theme = createTheme()
 
 export function ThemeProvider({ children }: { children: ReactNode }) {
   return (
